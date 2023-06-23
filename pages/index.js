b2ed751bef8b94ef7bab5d235dcacde8d1961f27
gulnisademir <134624631+gulnisademirr@users.noreply.github.com>
2023-06-23 12:24:34 +0300

    nextjs-tailwindcss-testProject

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import Layout from "../components/Layout";
+import {useState} from "react";
+import Pokemon from "../components/Pokemon";
+
+export default function Home({initialPokemon}) {
+
+    const [pokemon, setPokemon] = useState(initialPokemon)
+    
+
+
+    return (
+        <Layout>
+         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-10">
+                {pokemon.results.map((monster, index) => (
+               <Pokemon key={index} pokemon={monster} index={index}/>
+                ))}
+            </div>
+
+    
+        </Layout>
+    )
+}
+export async function getStaticProps(context) {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon")
+    const initialPokemon = await response.json()
+
+    return {
+        props: {
+            initialPokemon
+        }
+    }
+}
+
